9fd3b2cbbd832281bfe1bda5c8ebed52468e01e5
digital@paraminnovation.org <digital@paraminnovation.org>
2024-03-06 17:51:35 +0530

    changed url

--- src/App.js
+++ src/App.js
@@ -1,3 +1,55 @@
+// import './App.css';
+// import axios from "axios";
+// import { Navigate } from "react-router-dom";
+// import { useState, useEffect } from 'react';
+// import EventForm from './Admin/Admin';
+// import Signup from './signUp/Signup';
+// import Login from './login/Loginuser';
+// import { MyContext } from './context';
+
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// function App() {
+//   const [user, setUser] = useState(null);
+
+//   const getUser = async () => {
+//     try {
+//       const url = `${process.env.REACT_APP_BACKEND_URL}/api/auth/login/success`;
+//       const { data } = await axios.get(url, { withCredentials: true });
+//       console.log(data)
+//       setUser(data.user._json);
+//     } catch (err) {
+//       console.log(err);
+//     }
+//   };
+
+//   useEffect(() => {
+//     getUser();
+//   }, []);
+//   return (
+//     <div className="App">
+//       <BrowserRouter>
+//         <MyContext.Provider value={{ user, setUser }}>
+//           <Routes>
+
+//             {
+//               console.log(user)}
+//             <Route path="/" element={user ? <EventForm user={user} /> : <Navigate to="/login" />} />
+//             <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
+//             <Route path="/signup" element={user ? <Navigate to="/" /> : <Signup />} />
+           
+//           </Routes>
+//         </MyContext.Provider>
+
+//       </BrowserRouter>
+
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
 import './App.css';
 import axios from "axios";
 import { Navigate } from "react-router-dom";
@@ -7,36 +59,24 @@ import Signup from './signUp/Signup';
 import Login from './login/Loginuser';
 import { MyContext } from './context';
 
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   const [user, setUser] = useState(null);
 
-  const getUser = async () => {
-    try {
-      const url = `${process.env.REACT_APP_BACKEND_URL}/api/auth/login/success`;
-      const { data } = await axios.get(url, { withCredentials: true });
-      console.log(data)
-      setUser(data.user._json);
-    } catch (err) {
-      console.log(err);
-    }
-  };
 
-  useEffect(() => {
-    getUser();
-  }, []);
   return (
     <div className="App">
       <BrowserRouter>
         <MyContext.Provider value={{ user, setUser }}>
           <Routes>
 
-            {
-              console.log(user)}
-            <Route path="/" element={user ? <EventForm user={user} /> : <Navigate to="/login" />} />
-            <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
-            <Route path="/signup" element={user ? <Navigate to="/" /> : <Signup />} />
+            
+            {/* <Route path="/" element={<Signup/>} /> */}
+            
+            <Route path="/" element={<Login/>} />
+            <Route path='/Dashboard' element={<EventForm/>}/>
             
           </Routes>
         </MyContext.Provider>
@@ -48,3 +88,4 @@ function App() {
 }
 
 export default App;
+
