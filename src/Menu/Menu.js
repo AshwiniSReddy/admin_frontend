6768455a5a2a949c0e29e6a5747b466241088e18
digital@paraminnovation.org <digital@paraminnovation.org>
2024-03-11 15:54:09 +0530

    changed alert page

--- src/Menu/Menu.js
+++ src/Menu/Menu.js
@@ -7,7 +7,7 @@ import React from 'react'
 
 function Menubar() {
 
-    const { user, setUser,alert,setalert } = useContext(MyContext);
+    const { user, setUser, alert, setalert } = useContext(MyContext);
     console.log(user)
     const handleAddClick = () => {
         setalert(true); // This will now trigger the display of AlertComponent
@@ -15,12 +15,15 @@ function Menubar() {
     const handleHomeClick = () => {
         setalert(false); // This will now trigger the display of AlertComponent
     };
-    
+
     return (
         <Menu>
-            <div id="home" className="menu-item" href="/"><h3>Welcome  {user&&user.given_name}</h3></div>
-           <button onClick={handleAddClick}>alert</button>
-           <button onClick={handleHomeClick}>Home</button>
+            <div id="home" className="menu-item" href="/"><h3>Welcome  {user && user.given_name}</h3></div>
+            <div  className='menuButtons'>
+                <button onClick={handleAddClick} className='buttons'>Edit/Delete alert</button>
+                <button onClick={handleHomeClick} className='buttons'>Highlights</button>
+            </div>
+
         </Menu>
     )
 }
